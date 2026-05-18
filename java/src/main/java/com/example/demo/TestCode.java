47b10800115f78e21b87af546a796a5185040897
arindam podder <iampodder@gmail.com>
2026-05-19 02:12:46 +0530

    practice

---
+++ java/src/main/java/com/example/demo/TestCode.java
@@ -0,0 +1,44 @@
+package com.example.demo;
+
+import java.util.List;
+
+public class TestCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Code.subsequenceUseBit(new int[]{1,2,3});
+		
+	}
+
+}
+
+
+class Code{
+	
+	//using bit 
+	public static void subsequenceUseBit(int arr[]) {
+		int len = arr.length;
+		for(int i=0; i<Math.pow(2, len); i++) {		// Math.pow(2, len) = 1<<len
+			StringBuilder sb = new StringBuilder();
+			for(int idx=0; idx<arr.length; idx++) {
+				//check idx bit set or not 
+				if( (i & (1<<idx)) != 0 ) sb.append(arr[idx]);
+			}
+			System.out.println( sb.toString() );
+		}
+	}
+	
+	//using recursion 
+	public static void subsequenceUseRecur(int i, int[] arr, List<Integer> cur) {
+		
+	}
+	
+}
+
+
+
+
+
+
+
